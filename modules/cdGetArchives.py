671ed2fe40f2ba99598af9921da17bb1f97b9e15
Grant Atkins <gatkins@Grants-MBP.home>
2017-07-11 00:11:37 -0400

    Pep8 refactor for getArchives. JSON output updated

--- modules/cdGetArchives.py
+++ modules/cdGetArchives.py
@@ -1,29 +1,34 @@
 import time
-import urllib.request, urllib.error, urllib.parse
+import urllib.request
+import urllib.error
+import urllib.parse
 import sys
 import calendar
 import requests
 import json
 import logging
 
-moduleTag="Archives"
+moduleTag = "Archives"
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) \
+    Gecko/20100101 Firefox/48.0'}
+
 
 def getMementos(uri):
 
     uri = uri.replace(' ', '')
 
-    #baseURI = 'http://timetravel.mementoweb.org/timemap/link/'
-    #baseURI = 'http://mementoweb.org/timemap/link/'
+    # baseURI = 'http://timetravel.mementoweb.org/timemap/link/'
+    # baseURI = 'http://mementoweb.org/timemap/link/'
     # baseURI = 'http://mementoproxy.cs.odu.edu/aggr/timemap/link/1/'
-    #OR
+    # OR
     baseURI = 'http://memgator.cs.odu.edu/timemap/json/'
     memento_list = []
 
     try:
-        search_results = urllib.request.urlopen(baseURI+uri)
-        the_page = search_results.read().decode('ascii','ignore')
+        search_results = urllib.request.urlopen(baseURI + uri)
+        the_page = search_results.read().decode('ascii', 'ignore')
 
         data = json.loads(the_page)
 
@@ -34,7 +39,8 @@ def getMementos(uri):
             timestamp = item["datetime"]
             mementoURL = item["uri"]
 
-            epoch = int(calendar.timegm(time.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')))
+            epoch = int(calendar.timegm(
+                time.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')))
             day_string = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(epoch))
 
             memento["time"] = day_string
@@ -45,7 +51,7 @@ def getMementos(uri):
             memento["link"] = mementoURL
 
             # assumption that first memento is youngest - ON - start
-            if( name.netloc not in mementoNames ):
+            if(name.netloc not in mementoNames):
                 memento_list.append(memento)
                 mementoNames.append(name.netloc)
 
@@ -56,30 +62,33 @@ def getMementos(uri):
 
 
 def getRealDate(url, memDate):
+    '''Expects epoch date as parameter'''
     try:
-        response = requests.get(url,headers=headers)
+        response = requests.get(url, headers=headers)
         page = response.headers
         date = ""
 
         if "X-Archive-Orig-last-modified" in page:
-            date=page["X-Archive-Orig-last-modified"]
-        elif 'X-Archive-Orig-date' in page:
-            date=page['X-Archive-Orig-date']
+            date = page["X-Archive-Orig-last-modified"]
 
-        if(date ==""):
-            date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(memDate))    
+        if(date == ""):
+            date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(memDate))
         else:
-            epoch = int(calendar.timegm(time.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')))
+            epoch = int(calendar.timegm(time.strptime(
+                date, '%a, %d %b %Y %H:%M:%S %Z')))
             date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(epoch))
-        
-        return date  
+
+        return date
     except Exception:
         date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(memDate))
         return date
 
 
-def getArchives(url, outputArray, outputArrayIndex,verbose=False,**kwargs):
-    
+def getArchives(url, outputArray, outputArrayIndex, verbose=False, **kwargs):
+    '''
+    Return dictionary with earliest date, unique archives, or an empty
+    dictionary if no mementos
+    '''
     try:
         mementos = getMementos(url)
 
@@ -88,37 +97,40 @@ def getArchives(url, outputArray, outputArrayIndex,verbose=False,**kwargs):
             result["Earliest"] = ""
             result["By_Archive"] = {}
             outputArray[outputArrayIndex] = result["Earliest"]
-            logging.debug ("Done Archives 0")
+            kwargs['displayArray'][outputArrayIndex] = result
+            logging.debug("Done Archives 0")
             return result
 
         archives = {}
 
         for memento in mementos:
-            epoch = int(calendar.timegm(time.strptime(memento["time"], '%Y-%m-%dT%H:%M:%S')))
+            epoch = int(calendar.timegm(time.strptime(
+                memento["time"], '%Y-%m-%dT%H:%M:%S')))
             if(memento["name"] not in archives):
-                archives[memento["name"]] = {"link":memento["link"], "time": epoch}
+                archives[memento["name"]] = {
+                    "link": memento["link"], "time": epoch}
             else:
-                if(epoch<archives[memento["name"]]["time"]):
+                if(epoch < archives[memento["name"]]["time"]):
                     archives[memento["name"]]["time"] = epoch
                     archives[memento["name"]]["link"] = memento["link"]
 
-
-
         lowest = 99999999999
 
-        limitEpoch = int(calendar.timegm(time.strptime("1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
-
+        limitEpoch = int(calendar.timegm(time.strptime(
+            "1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
 
         for archive in archives:
-            date = getRealDate(archives[archive]["link"],archives[archive]["time"])
-            epoch = int(calendar.timegm(time.strptime(date, '%Y-%m-%dT%H:%M:%S')))
-            
-            if(epoch<limitEpoch):
+            date = getRealDate(
+                archives[archive]["link"], archives[archive]["time"])
+            epoch = int(calendar.timegm(
+                time.strptime(date, '%Y-%m-%dT%H:%M:%S')))
+
+            if(epoch < limitEpoch):
                 archives[archive]["time"] = ""
                 continue
 
             archives[archive]["time"] = date
-            if(epoch<lowest):
+            if(epoch < lowest):
                 lowest = epoch
 
         lowest = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(lowest))
@@ -128,7 +140,7 @@ def getArchives(url, outputArray, outputArrayIndex,verbose=False,**kwargs):
         result["Earliest"] = lowest
         result2 = {}
         for archive in archives:
-            if(archives[archive]["time"]==""):
+            if(archives[archive]["time"] == ""):
                 continue
             result2[archives[archive]["link"]] = str(archives[archive]["time"])
 
@@ -136,16 +148,16 @@ def getArchives(url, outputArray, outputArrayIndex,verbose=False,**kwargs):
 
         outputArray[outputArrayIndex] = result["Earliest"]
         kwargs['displayArray'][outputArrayIndex] = result
-        logging.debug ("Done Archives 1")
+        logging.debug("Done Archives 1")
         return result
 
     except:
-        logging.exception (sys.exc_info())
+        logging.exception(sys.exc_info())
         result = {}
         result["Earliest"] = ""
         result["By_Archive"] = {}
 
         outputArray[outputArrayIndex] = result["Earliest"]
         kwargs['displayArray'][outputArrayIndex] = result
-        logging.debug ("Done Archives 2")
+        logging.debug("Done Archives 2")
         return result
