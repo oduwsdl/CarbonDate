3f0589f7c0283eb6c5880612ad375bc9b1ad91e0
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-07-12 15:13:37 -0400

    Pep8 refactor Backlinks and FirstAppearance

--- modules/cdGetBacklinks.py
+++ modules/cdGetBacklinks.py
@@ -1,79 +1,78 @@
 import sys
-import os
-
 from .cdGetGoogle import mimicBrowser
 from .cdGetFirstAppearanceInArchives import getFirstAppearance
-
 import calendar
 import time
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import logging
 
-moduleTag="Backlinks"
-entry="getBacklinksFirstAppearanceDates"
+moduleTag = "Backlinks"
+entry = "getBacklinksFirstAppearanceDates"
 
 
 def getBacklinks(url):
-	inlinks = []
-	url = urllib.parse.quote(url, '')
-	try:	
-		query = 'https://www.google.com/search?hl=en&tbo=d&tbs=qdr:y15&q=link:'+url+'&oq=link:'+url
-		
-		page = mimicBrowser(query)
+    inlinks = []
+    url = urllib.parse.quote(url, '')
+    try:
+        query = ('https://www.google.com/search?hl=en&tbo=d&tbs=qdr:y15&'
+                 'q=link:' + url + '&oq=link:' + url)
 
-		loc = 0	
-		#print page
-		page=page.replace('<h3 class=r>','<h3 class="r">')
-		while(True):
-			start_str = '<h3 class="r"><a href="'
-			loc = page.find(start_str,loc)
+        page = mimicBrowser(query)
 
-			#print "loc: "
-			#print loc			
+        loc = 0
+        # print page
+        page = page.replace('<h3 class=r>', '<h3 class="r">')
+        while(True):
+            start_str = '<h3 class="r"><a href="'
+            loc = page.find(start_str, loc)
 
-			if(loc==-1):
-				break
-			fin = page.find('"', loc+len(start_str)+1)
-			url = page[loc+len(start_str):fin]
-			inlinks.append(url)
-			loc = fin
-	except:
-		logging.exception ( 'cdGetBacklinks :',sys.exc_info())
+            if(loc == -1):
+                break
+            fin = page.find('"', loc + len(start_str) + 1)
+            url = page[loc + len(start_str):fin]
+            inlinks.append(url)
+            loc = fin
+    except:
+        logging.exception('cdGetBacklinks :', sys.exc_info())
 
-	return inlinks
+    return inlinks
 
 
-def getBacklinksFirstAppearanceDates(url, outputArray, outputArrayIndex,verbose=False, **kwargs):
+def getBacklinksFirstAppearanceDates(url, outputArray, outputArrayIndex,
+                                     verbose=False, **kwargs):
 
-	links = getBacklinks(url)
+    links = getBacklinks(url)
 
-	lowest_epoch = 99999999999
-	limitEpoch = int(calendar.timegm(time.strptime("1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
-	try:
-		for link in links:
-			datestamp = getFirstAppearance(url, link)
+    lowest_epoch = 99999999999
+    limitEpoch = int(calendar.timegm(time.strptime(
+        "1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
+    try:
+        for link in links:
+            datestamp = getFirstAppearance(url, link)
 
-			if(datestamp==""):
-				continue
+            if(datestamp == ""):
+                continue
 
-			epoch = int(calendar.timegm(time.strptime(datestamp, '%Y-%m-%dT%H:%M:%S')))
+            epoch = int(calendar.timegm(
+                time.strptime(datestamp, '%Y-%m-%dT%H:%M:%S')))
 
-			if(epoch<limitEpoch):
-				continue
+            if(epoch < limitEpoch):
+                continue
 
-			if(epoch<lowest_epoch):
-				lowest_epoch = epoch
-	except:
-		logging.exception ( 'cdGetBacklinks :', sys.exc_info() )
+            if(epoch < lowest_epoch):
+                lowest_epoch = epoch
+    except:
+        logging.exception('cdGetBacklinks :', sys.exc_info())
 
-	if(lowest_epoch == 99999999999):
-		outputArray[outputArrayIndex] = ""
-		logging.debug ( "Done Backlinks*" )
-		return ""
-	
-	timeVal = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(lowest_epoch))
-	outputArray[outputArrayIndex] = timeVal
-	kwargs['displayArray'][outputArrayIndex] = timeVal
-	logging.debug ( "Done Backlinks" )
-	return timeVal
+    if(lowest_epoch == 99999999999):
+        outputArray[outputArrayIndex] = ""
+        logging.debug("Done Backlinks*")
+        return ""
 
+    timeVal = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(lowest_epoch))
+    outputArray[outputArrayIndex] = timeVal
+    kwargs['displayArray'][outputArrayIndex] = timeVal
+    logging.debug("Done Backlinks")
+    return timeVal
--- modules/cdGetFirstAppearanceInArchives.py
+++ modules/cdGetFirstAppearanceInArchives.py
@@ -1,100 +1,106 @@
-import re
 import time
-import os
-import sys, traceback
-import datetime
-import json
+import sys
 import calendar
 import requests
 import math
-
 import logging
-from datetime import datetime
 from .cdGetArchives import getMementos
 
 headers = {
-		'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		'Accept-Language': 'en-US,en;q=0.5',
-		'Accept-Encoding': 'gzip, deflate',
-		'Connnection': 'keep-alive',
-		'Cache-Control':'max-age=0'	
-		}
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) '
+    'Gecko/20100101 Firefox/48.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;'
+    'q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connnection': 'keep-alive',
+    'Cache-Control': 'max-age=0'
+}
+
 
 def getWebpage(url):
-	try:
-		response = requests.get(url, headers=headers)
-		return response
-	except:
-		return None
+    try:
+        response = requests.get(url, headers=headers)
+        return response
+    except:
+        return None
 
-def isInPage(url,page):
 
-	page = getWebpage(page)
-	if page is None:
-		return False,''
+def isInPage(url, page):
 
-	loc = page.text.find(url)
-	
-	date = ""
-	if(loc==-1):
-		return False, date
+    page = getWebpage(page)
+    if page is None:
+        return False, ''
 
-	if "X-Archive-Orig-last-modified" in page.headers:
-		date=page.headers["X-Archive-Orig-last-modified"]
-	elif 'X-Archive-Orig-date' in page.headers:
-		date=page.headers['X-Archive-Orig-date']
+    loc = page.text.find(url)
 
-	if date != "" :
-		epoch = int(calendar.timegm(time.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')))
-		date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(epoch))
-		return True, date
-	else:	
-		return False, ""
+    date = ""
+    if(loc == -1):
+        return False, date
+
+    if "X-Archive-Orig-last-modified" in page.headers:
+        date = page.headers["X-Archive-Orig-last-modified"]
+    elif 'X-Archive-Orig-date' in page.headers:
+        date = page.headers['X-Archive-Orig-date']
+
+    if date != "":
+        epoch = int(calendar.timegm(time.strptime(
+            date, '%a, %d %b %Y %H:%M:%S %Z')))
+        date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(epoch))
+        return True, date
+    else:
+        return False, ""
+
 
 def getFirstAppearance(url, inurl):
-	
-	try:
-		mementos = getMementos(inurl)
 
-		if(len(mementos) == 0):
-			return ""
-		
-		#binary search to find earliest backlink that contain url (early one may not have the back link)
-		start = 0
-		end = len(mementos)
-		previous = -1
-		i = 0
-		foundbefore = False
-		#count = 0
+    try:
+        mementos = getMementos(inurl)
 
-		#binary search for first appearance re-implemented by Neo
-		date=""
-		while (True):
-			res, date = isInPage(url, mementos[int(i)]["link"])
-			#special case: only one version of memento bitween other two versions have this reference (the link might be removed from page later)
-			if( (res==True and int(math.fabs(previous-i))==1 and foundbefore == False) or (res==False and int(math.fabs(previous-i))==1 and foundbefore == True) ):
-				return date
+        if(len(mementos) == 0):
+            return ""
 
-			previous = i
+        # binary search to find earliest backlink that contain url
+        # (early one may not have the back link)
+        start = 0
+        end = len(mementos)
+        previous = -1
+        i = 0
+        foundbefore = False
 
-			if res==True:
-				#The appearance found, that means the link appeared in the earlier record
-				end=i
-				i=int((end-start)/2)+start
-				foundbefore = False
-			else:
-				#The appearance didn't found, that means the link appeared in the later record
-				start=i
-				i=int((end - start)/2)+start
-				foundbefore = True
+        # binary search for first appearance re-implemented by Neo
+        date = ""
+        while (True):
+            res, date = isInPage(url, mementos[int(i)]["link"])
+            # special case: only one version of memento bitween other two
+            # versions have this reference (the link might be removed from page
+            # later)
+            if((res is True and int(math.fabs(previous - i)) == 1 and
+               foundbefore is False) or
+               (res is False and int(math.fabs(previous - i)) == 1 and
+               foundbefore is True)):
+                return date
 
-			#The search range has merged together, we get the result
-			if end == (start+1):
-				return date
+            previous = i
 
-	except:
+            if res is True:
+                # The appearance found, that means the link appeared
+                # in the earlier record
+                end = i
+                i = int((end - start) / 2) + start
+                foundbefore = False
+            else:
+                # The appearance didn't found, that means the link
+                # appeared in the later record
+                start = i
+                i = int((end - start) / 2) + start
+                foundbefore = True
 
-		logging.exception ( sys.exc_info() )
-		return ""
+            # The search range has merged together, we get the result
+            if end == (start + 1):
+                return date
 
+    except:
+
+        logging.exception(sys.exc_info())
+        return ""
