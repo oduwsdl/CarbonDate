6893f58d6e4527af07ff6aa3af0309b59cb7c503
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-07-12 15:11:31 -0400

    Pep8 refactor for external APIs

--- modules/cdGetBitly.py
+++ modules/cdGetBitly.py
@@ -1,123 +1,130 @@
 import os
 import sys
-import datetime
-import urllib.request, urllib.parse, urllib.error
 import json
 import time
 import subprocess
 import calendar
-import json
 import os.path
 import logging
 
-moduleTag="Bitly.com"
-
-def GetBitlyJson(URL):	
-
-	ACCESS_TOKENs = []
-	try:
-		access_token_env=os.getenv('CD_Bitly_token')
-		if access_token_env is not None:
-			logging.warning ( 'cdGetBitly: Access token is found in environment variable, overwite local config values.' )
-			ACCESS_TOKENs=[access_token_env]
-		else:
-			fileConfig = open(os.path.dirname(__file__)+"/../config", "r")
-			config = fileConfig.read()
-			fileConfig.close()
-
-			ACCESS_TOKENs = json.loads(config)
-			ACCESS_TOKENs = ACCESS_TOKENs['AccessToken']
-	except:
-		logging.exception ( 'cdGetBitly: %s', sys.exc_info() )
-		return ''
-
-	if( len(ACCESS_TOKENs) == 0 ):
-		logging.warning ( 'cdGetBitly.py::GetBitlyJson(), ACCESS_TOKENs empty' )
-		return ''
-	elif( ACCESS_TOKENs[0] == 'YourBitlyAccessTokenHere' ):
-		logging.warning ( 'cdGetBitly.py::GetBitlyJson(), please set bitly access token in config' )
-		return ''
-	
-	jsonData = ""
-	for access_token in ACCESS_TOKENs:
-		
-		URL = URL.replace("ACCESS_TOKEN", access_token)
-		command = 'curl --silent -L -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30" "'+URL+'"'
-		
-		page = subprocess.getoutput(command)
-		page = page.encode('ascii', 'ignore')
-		
-		if(page.find(b'"error": "NOT_FOUND"')!=-1):
-			break
-
-		jsonData = json.loads(page.decode())
-
-		if(jsonData['status_code']==403):
-			continue
-	return jsonData
-
-def getBitly(url, outputArray, indexOfOutputArray,verbose=False,**kwargs):
-	try:
-		
-		# Get aggregated url
-		
-		URL = "https://api-ssl.bitly.com/v3/link/lookup?access_token=ACCESS_TOKEN&url="+url
-		jsonData = GetBitlyJson(URL)
-		
-		if len(jsonData) < 1:
-			return ""
-
-		if(jsonData['status_code']!=200):
-			outputArray[indexOfOutputArray] = "Bitly Key has expired"
-			kwargs['displayArray'][indexOfOutputArray] = ""
-			logging.debug ( "Done Bitly" )
-			return "Bitly Key has expired"
-
-		if(jsonData =="" or ('error' in jsonData['data']['link_lookup'][0]  and jsonData['data']['link_lookup'][0]['error']=='NOT_FOUND')):
-			outputArray[indexOfOutputArray] = ""
-			kwargs['displayArray'][indexOfOutputArray] = ""
-			logging.debug ( "Done Bitly" )
-			return ""
-		url = jsonData['data']['link_lookup'][0]['aggregate_link']
-
-		# Get creation timestamp
-		URL = "https://api-ssl.bitly.com/v3/info?access_token=ACCESS_TOKEN&shortUrl="+url
-		jsonData = GetBitlyJson(URL)
-
-		if(jsonData['status_code']!=200):
-			outputArray[indexOfOutputArray] = "Bitly Key has expired"
-			kwargs['displayArray'][indexOfOutputArray] = ""
-			logging.debug ( "Done Bitly" )
-			return "Bitly Key has expired"
-	
-		if(jsonData['data'] == None or 'created_at' not in jsonData['data']['info'][0]):
-			outputArray[indexOfOutputArray] = ""
-			kwargs['displayArray'][indexOfOutputArray] = ""
-			logging.debug ( "Done Bitly" )
-			return ""
-		epoch = jsonData['data']['info'][0]['created_at']
-
-		limitEpoch = int(calendar.timegm(time.strptime("1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
-		if(epoch<limitEpoch):
-			outputArray[indexOfOutputArray] = ""
-			kwargs['displayArray'][indexOfOutputArray] = ""
-			logging.debug ( "Done Bitly" )
-			return ""
-		
-	
-		creation_date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(epoch))
+moduleTag = "Bitly.com"
 
 
-		
-		outputArray[indexOfOutputArray] = creation_date
-		kwargs['displayArray'][indexOfOutputArray] = creation_date
-		logging.debug ( "Done Bitly" )
-		return str(creation_date)
-	
-	except:
-		logging.exception('cdGetBitly: %s', sys.exc_info())
-		outputArray[indexOfOutputArray] = ""
-		kwargs['displayArray'][indexOfOutputArray] = ""
-		logging.debug("Done Bitly")
-		return ""
+def GetBitlyJson(URL):
 
+    ACCESS_TOKENs = []
+    try:
+        access_token_env = os.getenv('CD_Bitly_token')
+        if access_token_env is not None:
+            logging.debug(
+                'cdGetBitly: Access token is found in environment variable, '
+                'overwite local config values.')
+            ACCESS_TOKENs = [access_token_env]
+        else:
+            fileConfig = open(os.path.dirname(__file__) + "/../config", "r")
+            config = fileConfig.read()
+            fileConfig.close()
+
+            ACCESS_TOKENs = json.loads(config)
+            ACCESS_TOKENs = ACCESS_TOKENs['AccessToken']
+    except:
+        logging.exception('cdGetBitly: %s', sys.exc_info())
+        return ''
+
+    if(len(ACCESS_TOKENs) == 0):
+        logging.debug('cdGetBitly.py::GetBitlyJson(), ACCESS_TOKENs empty')
+        return ''
+    elif(ACCESS_TOKENs[0] == 'YourBitlyAccessTokenHere'):
+        logging.debug(
+            'cdGetBitly.py::GetBitlyJson(), please set bitly '
+            'access token in config')
+        return ''
+
+    jsonData = ""
+    for access_token in ACCESS_TOKENs:
+
+        URL = URL.replace("ACCESS_TOKEN", access_token)
+        command = ('curl --silent -L -A "Mozilla/5.0 (Macintosh; Intel Mac OS '
+                   'X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) '
+                   'Chrome/12.0.742.112 Safari/534.30" "' + URL + '"')
+
+        page = subprocess.getoutput(command)
+        page = page.encode('ascii', 'ignore')
+
+        if(page.find(b'"error": "NOT_FOUND"') != -1):
+            break
+
+        jsonData = json.loads(page.decode())
+
+        if(jsonData['status_code'] == 403):
+            continue
+
+    return jsonData
+
+
+def getBitly(url, outputArray, indexOfOutputArray, verbose=False, **kwargs):
+    try:
+
+        # Get aggregated url
+
+        URL = ('https://api-ssl.bitly.com/v3/link/lookup?'
+               'access_token=ACCESS_TOKEN&url=' + url)
+        jsonData = GetBitlyJson(URL)
+
+        if len(jsonData) < 1:
+            return ""
+
+        if(jsonData['status_code'] != 200):
+            outputArray[indexOfOutputArray] = "Bitly Key has expired"
+            kwargs['displayArray'][indexOfOutputArray] = ""
+            logging.debug("Done Bitly")
+            return "Bitly Key has expired"
+
+        if(jsonData == "" or ('error' in jsonData['data']['link_lookup'][0] and
+           jsonData['data']['link_lookup'][0]['error'] == 'NOT_FOUND')):
+            outputArray[indexOfOutputArray] = ""
+            kwargs['displayArray'][indexOfOutputArray] = ""
+            logging.debug("Done Bitly")
+            return ""
+        url = jsonData['data']['link_lookup'][0]['aggregate_link']
+
+        # Get creation timestamp
+        URL = ('https://api-ssl.bitly.com/v3/info?access_token=ACCESS_TOKEN'
+               '&shortUrl=' + url)
+        jsonData = GetBitlyJson(URL)
+
+        if(jsonData['status_code'] != 200):
+            outputArray[indexOfOutputArray] = "Bitly Key has expired"
+            kwargs['displayArray'][indexOfOutputArray] = ""
+            logging.debug("Done Bitly")
+            return "Bitly Key has expired"
+
+        if(jsonData['data'] is None or
+           'created_at' not in jsonData['data']['info'][0]):
+            outputArray[indexOfOutputArray] = ""
+            kwargs['displayArray'][indexOfOutputArray] = ""
+            logging.debug("Done Bitly")
+            return ""
+        epoch = jsonData['data']['info'][0]['created_at']
+
+        limitEpoch = int(calendar.timegm(time.strptime(
+            "1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
+        if(epoch < limitEpoch):
+            outputArray[indexOfOutputArray] = ""
+            kwargs['displayArray'][indexOfOutputArray] = ""
+            logging.debug("Done Bitly")
+            return ""
+
+        creation_date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(epoch))
+
+        outputArray[indexOfOutputArray] = creation_date
+        kwargs['displayArray'][indexOfOutputArray] = creation_date
+        logging.debug("Done Bitly")
+        return str(creation_date)
+
+    except:
+        logging.exception('cdGetBitly: %s', sys.exc_info())
+        outputArray[indexOfOutputArray] = ""
+        kwargs['displayArray'][indexOfOutputArray] = ""
+        logging.debug("Done Bitly")
+        return ""
--- modules/cdGetGoogle.py
+++ modules/cdGetGoogle.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import traceback
 import calendar
 import time
 import requests
@@ -8,158 +7,158 @@ from .cdGetLowest import getLowest
 from random import randint
 import logging
 
-moduleTag='Google.com'
+moduleTag = 'Google.com'
+
 
 def randSleep():
-	sleepSeconds = randint(2, 7)
-	logging.debug ( 'cdGetGoogle::randSleep(), sleep: %s', sleepSeconds )
-	time.sleep(sleepSeconds)
+    sleepSeconds = randint(2, 7)
+    logging.debug('cdGetGoogle::randSleep(), sleep: %s', sleepSeconds)
+    time.sleep(sleepSeconds)
+
 
 def getLowestDate(allDatesEpoch):
 
-	if( len(allDatesEpoch) == 0 ):
-		return 0
+    if(len(allDatesEpoch) == 0):
+        return 0
 
-	lowest_date = 99999999999
-	
-	for epoch in allDatesEpoch:
-		#epoch = int(calendar.timegm(time.strptime(timestamp, '%b %d, %Y')))
-		limitEpoch = int(calendar.timegm(time.strptime("1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
-		if(epoch<limitEpoch):
-			continue
+    lowest_date = 99999999999
 
-		if(epoch<lowest_date):
-			lowest_date = epoch
+    for epoch in allDatesEpoch:
+        limitEpoch = int(calendar.timegm(time.strptime(
+            "1995-01-01T12:00:00", '%Y-%m-%dT%H:%M:%S')))
+        if(epoch < limitEpoch):
+            continue
 
-	inurl_creation_date = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(lowest_date))
-	return inurl_creation_date
+        if(epoch < lowest_date):
+            lowest_date = epoch
+
+    inurl_creation_date = time.strftime(
+        '%Y-%m-%dT%H:%M:%S', time.gmtime(lowest_date))
+    return inurl_creation_date
+
 
 def getTimestampFromSERP(signatureString, locationOfSignature, page):
 
-	#retrieve date from preceding " - </span>" signature - start
-	if( len(page) == 0 or len(signatureString) == 0 ):
-		return '', -1
+    # retrieve date from preceding " - </span>" signature - start
+    if(len(page) == 0 or len(signatureString) == 0):
+        return '', -1
 
-	timestamp = ''
+    timestamp = ''
 
-	k = locationOfSignature
-	while 1==1 and k > -1:
-		#end marker
-		if page[k] != '>' :
-			timestamp = page[k] + timestamp
-		else :
-			break
-		k = k - 1;
-	#shift search cursor
-	locationOfSignature = locationOfSignature + len(signatureString)
-	timestamp = timestamp.strip()
-	
-	return timestamp, locationOfSignature
-	#retrieve date from preceding " - </span>" signature - end
+    k = locationOfSignature
+    while 1 == 1 and k > -1:
+        # end marker
+        if page[k] != '>':
+            timestamp = page[k] + timestamp
+        else:
+            break
+        k = k - 1
+    # shift search cursor
+    locationOfSignature = locationOfSignature + len(signatureString)
+    timestamp = timestamp.strip()
+
+    return timestamp, locationOfSignature
+
 
 def mimicBrowser(query):
-	
-	try:
-		headers = {
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:38.0) Gecko/20100101 Firefox/38.0',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-		'Accept-Language': 'en-US,en;q=0.5',
-		'Accept-Encoding': 'gzip, deflate',
-		'Connnection': 'keep-alive',
-		'Cache-Control':'max-age=0'	
-		}
 
-		response = requests.get(query, headers=headers)
-		return response.text
-	except:
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; '
+            'rv:38.0) Gecko/20100101 Firefox/38.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+            '*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'Connnection': 'keep-alive',
+            'Cache-Control': 'max-age=0'
+        }
 
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		errorMessage = fname + ', ' + str(exc_tb.tb_lineno)  + ', ' + str(sys.exc_info())
-		logging.error ( '\tERROR:', errorMessage )
-		logging.error ( '\tquery is: ', query )
-		return ''
+        response = requests.get(query, headers=headers)
+        return response.text
+    except:
+
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        errorMessage = fname + ', ' + \
+            str(exc_tb.tb_lineno) + ', ' + str(sys.exc_info())
+        logging.error('\tERROR:', errorMessage)
+        logging.error('\tquery is: ', query)
+        return ''
+
 
 def genericGetCreationDate(query):
 
-	randSleep()
+    randSleep()
 
-	allDatesEpoch = []
-	page = ''
-	try:
-		#userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30'
-		#userAgent = 'Carbon Date test service to estimate creation date of a website. http://cd.cs.odu.edu/'
-		#page = commands.getoutput('curl --silent -L -A "'+userAgent+'" "'+query+'"')
+    allDatesEpoch = []
+    page = ''
+    try:
 
-		page = mimicBrowser(query)
-		
-		# debugOutfile = open('output.html', 'w')
-		# debugOutfile.write(str(page))
-		# debugOutfile.close()
+        page = mimicBrowser(query)
 
-		signatureString = ' - </span>'
-		locationOfSignature = 0
+        signatureString = ' - </span>'
+        locationOfSignature = 0
 
-		while(True):
-		
-			#this logic is meant to retrieve date from a string of form: ">DateIsHere- </span>"
-			locationOfSignature = page.find(signatureString, locationOfSignature)
-			timestamp = ''
-			
-			if locationOfSignature == -1:
-				break
-			else:
-				timestamp, locationOfSignature = getTimestampFromSERP(signatureString, locationOfSignature, page)
-				#print 'timestamp/locationOfSignature:', timestamp
+        while(True):
 
-				try:
-					epoch = int(calendar.timegm(time.strptime(timestamp, '%b %d, %Y')))
-					allDatesEpoch.append(epoch)
-				except:
-					pass
-	except:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		errorMessage = fname + ', ' + str(exc_tb.tb_lineno)  + ', ' + str(sys.exc_info())
-		logging.error ( '\tERROR:', errorMessage)
+            # this logic is meant to retrieve date from a string of form:
+            # ">DateIsHere- </span>"
+            locationOfSignature = page.find(
+                signatureString, locationOfSignature)
+            timestamp = ''
 
-	#print query
-	#for date in allDatesEpoch:
-	#	print date
-	#print
-	return getLowestDate(allDatesEpoch)
+            if locationOfSignature == -1:
+                break
+            else:
+                timestamp, locationOfSignature = getTimestampFromSERP(
+                    signatureString, locationOfSignature, page)
+                # print 'timestamp/locationOfSignature:', timestamp
 
-def getGoogle(url, outputArray, indexOfOutputArray,verbose=False, **kwargs):
-	
-	#Caution google blocks bots which do not play nice
-	#return ''
-	query = 'https://www.google.com/search?hl=en&tbo=d&tbs=qdr:y15&q=inurl:'+url+'&oq=inurl:'+url
-	inurl_creation_date = genericGetCreationDate(query)
+                try:
+                    epoch = int(calendar.timegm(
+                        time.strptime(timestamp, '%b %d, %Y')))
+                    allDatesEpoch.append(epoch)
+                except:
+                    pass
+    except:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        errorMessage = fname + ', ' + \
+            str(exc_tb.tb_lineno) + ', ' + str(sys.exc_info())
+        logging.error('\tERROR:', errorMessage)
 
-	#query = 'https://www.google.com/search?hl=en&tbo=d&tbs=qdr:y15&q='+url
-	#search_creation_date = genericGetCreationDate(query)
-	search_creation_date = 0
+    return getLowestDate(allDatesEpoch)
 
-	#print 'inurl_creation_date:', inurl_creation_date
-	#print 'search_creation_date:', search_creation_date
 
-	lowerDate = ''
-	if( inurl_creation_date != 0 and search_creation_date != 0 ):
+def getGoogle(url, outputArray, indexOfOutputArray, verbose=False, **kwargs):
 
-		lowerDate = getLowest([search_creation_date, inurl_creation_date])
-		outputArray[indexOfOutputArray] = lowerDate
-		kwargs['displayArray'][indexOfOutputArray] = lowerDate
+    # Caution google blocks bots which do not play nice
+    # return ''
+    query = ('https://www.google.com/search?hl=en&tbo=d&tbs=qdr:y15'
+             '&q=inurl:' + url + '&oq=inurl:' + url)
 
-	elif( inurl_creation_date == 0 and search_creation_date != 0 ):
+    inurl_creation_date = genericGetCreationDate(query)
 
-		lowerDate = getLowest([search_creation_date, search_creation_date])
-		outputArray[indexOfOutputArray] = lowerDate
-		kwargs['displayArray'][indexOfOutputArray] = lowerDate
+    search_creation_date = 0
 
-	elif( inurl_creation_date != 0 and search_creation_date == 0 ):
-		
-		lowerDate = getLowest([inurl_creation_date, inurl_creation_date])
-		outputArray[indexOfOutputArray] = lowerDate
-		kwargs['displayArray'][indexOfOutputArray] = lowerDate
-	
-	return lowerDate
+    lowerDate = ''
+    if(inurl_creation_date != 0 and search_creation_date != 0):
+
+        lowerDate = getLowest([search_creation_date, inurl_creation_date])
+        outputArray[indexOfOutputArray] = lowerDate
+        kwargs['displayArray'][indexOfOutputArray] = lowerDate
+
+    elif(inurl_creation_date == 0 and search_creation_date != 0):
+
+        lowerDate = getLowest([search_creation_date, search_creation_date])
+        outputArray[indexOfOutputArray] = lowerDate
+        kwargs['displayArray'][indexOfOutputArray] = lowerDate
+
+    elif(inurl_creation_date != 0 and search_creation_date == 0):
+
+        lowerDate = getLowest([inurl_creation_date, inurl_creation_date])
+        outputArray[indexOfOutputArray] = lowerDate
+        kwargs['displayArray'][indexOfOutputArray] = lowerDate
+
+    return lowerDate
