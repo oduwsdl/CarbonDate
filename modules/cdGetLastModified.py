2a52a49e78d1df65d805a19d7c1f91b9e19e7afa
Grant Atkins <gatkins@Grants-MacBook-Pro.local>
2017-07-12 14:51:27 -0400

    Pep8 refactor getLowest and lastModified

--- modules/cdGetLastModified.py
+++ modules/cdGetLastModified.py
@@ -6,8 +6,8 @@ import logging
 moduleTag = "Last Modified"
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0)\
-    Gecko/20100101 Firefox/48.0'}
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) '
+    'Gecko/20100101 Firefox/48.0'}
 
 
 def getLastModified(url, outputArray, indexOfOutputArray, verbose=False,
