2a806ad2b46d338c616a0d3bda05d32f9d3f96c7
Grant Atkins <gatki001@odu.edu>
2017-07-16 23:34:26 -0400

    test templates added for external apis

--- tests/test_externalAPIs.py
+++ tests/test_externalAPIs.py
@@ -1,8 +1,9 @@
 import pytest
-import modules.cdGetGoogle
-import modules.cdGetBitly
-import modules.cdGetBing
-import modules.cdGetTwitter
+import os
+# import modules.cdGetGoogle as google
+# import modules.cdGetBitly as bitly
+import modules.cdGetBing as bing
+import modules.cdGetTwitter as twitter
 
 
 @pytest.mark.skip(reason='not implemented')
@@ -10,18 +11,28 @@ def test_Google():
     pass
 
 
-@pytest.mark.skip(reason='not implemented')
+@pytest.mark.skipif(os.getenv('CD_Bitly_token') is None,
+                    reason="Missing Bitly key in environment")
 def test_Bitly():
     pass
 
 
-@pytest.mark.skip(reason='not implemented')
+@pytest.mark.skipif(os.getenv('CD_Bing_key') is None,
+                    reason="Missing Bing key in environment")
 def test_Bing():
+    testarry = ['']
+    bing.getBing("", testarry, 0, verbose=False, displayArray={"Bing": ""})
     pass
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_Twitter():
-    pass
+@pytest.mark.parametrize("uri, memDate", [
+    ("http://www.cs.odu.edu",
+     "2008-12-01T03:53:27")
+])
+def test_Twitter(uri, memDate):
+    testarry = ['']
+    date = twitter.getTwitter(uri, testarry, 0, verbose=True,
+                              displayArray={"Twitter": ""})
+    assert memDate == date
 
 # todo -- any external apis added should be tested here
