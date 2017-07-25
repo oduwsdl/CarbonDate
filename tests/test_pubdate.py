605606be8ccbe4062a515e28f6fb8c27c14318e6
Grant Atkins <gatki001@odu.edu>
2017-07-25 14:44:01 -0400

    Pubdate module fix. Pudate tests added

--- tests/test_pubdate.py
+++ tests/test_pubdate.py
@@ -2,9 +2,12 @@ import pytest
 import modules.cdGetPubdate as pub
 
 
-@pytest.mark.skip(reason='not implemented')
-def test_pubdate():
-    testarry = ['']
-    pub.getPubdate("", testarry, 0, verbose=False,
-                   displayArray={"Pubdate": ""})
-    pass
+@pytest.mark.parametrize("uri", [
+    ("http://www.cnn.com/2012/10/28/world/americas"
+     "/canada-earthquake/index.html"),
+    ("http://www.cnn.com/2011/10/28/living/ways-to-look-better-is/index.html"),
+])
+def test_pubdate(uri):
+    date = pub.getPubdate(uri, [''], 0, verbose=False,
+                          displayArray={"Pubdate": ""})
+    assert len(date) > 0
