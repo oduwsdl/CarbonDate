d0716cfa76d7c956a96a6e104cda96ecd29464d7
Grant Atkins <gatki001@odu.edu>
2017-07-25 16:28:01 -0400

    Update Pubdate module & tests. Coverage improvements

--- modules/cdGetPubdate.py
+++ modules/cdGetPubdate.py
@@ -42,9 +42,11 @@ def getPubdate(url, outputArray, indexOfOutputArray, verbose=False, **kwargs):
         response = requests.get(url, headers=headers)
     except Exception:
         logging.debug("cdGetPubdate: Error while downloading web page")
+        date = _extractFromURL(url)
         if date is not None:
             date_str = date.strftime('%Y-%m-%dT%H:%M:%S')
             outputArray[indexOfOutputArray] = date_str
+            kwargs['displayArray'][indexOfOutputArray] = date_str
             logging.debug("Done Pubdate")
             return date_str
         else:
@@ -62,101 +64,32 @@ def getPubdate(url, outputArray, indexOfOutputArray, verbose=False, **kwargs):
         httpEquiv = meta.get('http-equiv', '').lower()
         metaProperty = meta.get('property', '').lower()
 
-        # <meta name="pubdate" content="2015-11-26T07:11:02Z" >
-        if 'pubdate' == metaName:
-            metaDate = meta['content'].strip()
-            break
+        # Different types of meta attributes that can hold creation date.
+        # date should be the last entry
+        metaAttrs = set(['pubdate', 'publishdate', 'timestamp',
+                         'dc.date.issued',
+                         'article:published_time', 'bt:pubdate', 'og:pubdate',
+                         'sailthru.date', 'article.published',
+                         'published-date',
+                         'article.created', 'article_date_original',
+                         'cxenseparse:recs:publishtime', 'date_published',
+                         'datepublished', 'datecreated', 'date'])
 
-        # <meta name='publishdate' content='201511261006'/>
-        if 'publishdate' == metaName:
+        if metaName in metaAttrs:
             metaDate = meta['content'].strip()
             break
-
-        # <meta name="timestamp"  data-type="date"
-        # content="2015-11-25 22:40:25" />
-        if 'timestamp' == metaName:
+        elif itemProp in metaAttrs:
             metaDate = meta['content'].strip()
             break
-
-        # <meta name="DC.date.issued" content="2015-11-26">
-        if 'dc.date.issued' == metaName:
+        elif httpEquiv in metaAttrs:
             metaDate = meta['content'].strip()
             break
-
-        # <meta property="article:published_time"  content="2015-11-25" />
-        if 'article:published_time' == metaProperty:
-            metaDate = meta['content'].strip()
-            break
-        # <meta name="Date" content="2015-11-26" />
-        if 'date' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta property="bt:pubDate"
-        # content="2015-11-26T00:10:33+00:00">
-        if 'bt:pubdate' == metaProperty or 'og:pubdate' == metaProperty:
-            metaDate = meta['content'].strip()
-            break
-        # <meta name="sailthru.date"
-        # content="2015-11-25T19:56:04+0000" />
-        if 'sailthru.date' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta name="article.published"
-        # content="2015-11-26T11:53:00.000Z" />
-        if 'article.published' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta name="published-date" content="2015-11-26T11:53:00.000Z" />
-        if 'published-date' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta name="article.created"
-        # content="2015-11-26T11:53:00.000Z" />
-        if 'article.created' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta name="article_date_original"
-        # content="Thursday, November 26, 2015,  6:42 AM" />
-        if 'article_date_original' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta name="cXenseParse:recs:publishtime"
-        # content="2015-11-26T14:42Z"/>
-        if 'cxenseparse:recs:publishtime' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta name="DATE_PUBLISHED" content="11/24/2015 01:05AM" />
-        if 'date_published' == metaName:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta itemprop="datePublished"
-        # content="2015-11-26T11:53:00.000Z" />
-        if 'datepublished' == itemProp:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta itemprop="datePublished"
-        # content="2015-11-26T11:53:00.000Z" />
-        if 'datecreated' == itemProp:
-            metaDate = meta['content'].strip()
-            break
-
-        # <meta http-equiv="data" content="10:27:15 AM Thursday,
-        # November 26, 2015">
-        if 'date' == httpEquiv:
+        elif metaProperty in metaAttrs:
             metaDate = meta['content'].strip()
             break
 
     if metaDate is None:
-        date = date = _extractFromURL(url)
+        date = _extractFromURL(url)
 
     if metaDate is not None:
         date = parseStrDate(metaDate)
